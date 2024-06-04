8389bdc34657266a2fdf7ae9213d6f63cac26896
souvik sarkar <12345souvik12345@gmail.com>
2024-06-04 22:57:21 +0530

    Drizzle Added

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,11 @@
-import type { Config } from 'drizzle-kit';
 
+import { defineConfig } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL not found in environment');
-
-export default {
-  schema: './src/app/db/schema.ts',
+export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
-  strict: true,
-} satisfies Config;
+  schema: "./src/schema.ts",
+  out: "./migrations",
+});
